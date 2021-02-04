407ba2aeba073e2993dfb252c96362be6aa346b4
jz-lu <jlu@college.harvard.edu>
2021-02-03 22:59:05 -0600

    working kalloc
    '

--- k-alloc.cc
+++ k-alloc.cc
@@ -4,7 +4,10 @@
 static spinlock page_lock;
 static uintptr_t next_free_pa;
 
-bapg pg_map[MEMSIZE_PHYSICAL/PAGESIZE];
+// Buddy allocator data structure.
+bapg pgmap[MEMSIZE_PHYSICAL/PAGESIZE];
+list<bapg, &bapg::pglink> free_blocks[MAX_ORDER - MIN_ORDER + 1];
+
 
 // largest_fitting_ord(num, sz)
 //  returns largest power of 2 that fits inside of the given number
@@ -21,36 +24,56 @@ static uint64_t largest_fitting_ord(uint64_t num, int sz=1) {
     }
 }
 
+// order(sz)
+//   Equivalently ceiling(log_2(sz))
+static uint64_t order(uint64_t sz) {
+    uint64_t y = sz - 1;
+    uint64_t ord = 0;
+    while (sz >>= 1) ord++;
+
+    // If it is a power of 2 don't add 1.
+    if (!(y & (y+1))) {
+        return ord;
+    } else { // Otherwise add 1 for ceiling.
+        return ord;
+    }
+}
 
 
-// init_kalloc
+// init_kalloc()
 //    Initialize stuff needed by kalloc. Called from init_hardware,
 //    after physical_ranges is initialized.
 void init_kalloc() {
-    return; // TODO REMOVE
     auto range = physical_ranges.begin();
 
     while (range != physical_ranges.end()) {
         if (range->type() == mem_available) {
-
             uintptr_t curr_addr = range->first();
             uint64_t remaining_size = range->last() - curr_addr;
+            log_printf("NEW mem init at pa 0x%x with raw size 0x%x\n", curr_addr, remaining_size);
 
             while (remaining_size != 0) {
                 unsigned int ord = largest_fitting_ord(remaining_size);
                 assert(ord >= MIN_ORDER && ord <= MAX_ORDER);
                 uint64_t current_size = 1 << ord;
+                log_printf("Current addr: 0x%x, remaining size to cut: 0x%x\n", curr_addr, remaining_size);
+                log_printf("Current largest order = %lu, size: 0x%x\n", ord, current_size);
 
                 for (uint64_t i = curr_addr / PAGESIZE;  
-                    i < (curr_addr + current_size)/PAGESIZE;
+                    i < (curr_addr + current_size) / PAGESIZE;
                     i++) {
 
-                    pg_map[i].available = true;
-                    pg_map[i].free = true;
-                    pg_map[i].ord = ord;
-                    pg_map[i].r_ord = ord;
-                    pg_map[i].r_addr = curr_addr; // change this
+                    pgmap[i].free = true;
+                    pgmap[i].ord = ord;
+                    pgmap[i].r_ord = ord;
+                    pgmap[i].r_addr = curr_addr; // change this
                 }
+
+                // Push back the free blocks.
+                free_blocks[ord - MIN_ORDER].push_back(&pgmap[curr_addr/PAGESIZE]);
+                assert(!free_blocks[ord - MIN_ORDER].empty());
+                log_printf("PUSHED block of order %lu onto free list\n", ord);
+
                 curr_addr += current_size;
                 remaining_size -= current_size;
             }
@@ -61,27 +84,31 @@ void init_kalloc() {
 
 
 // kalloc(sz)
-//    Allocate and return a pointer to at least `sz` contiguous bytes of
-//    memory. Returns `nullptr` if `sz == 0` or on failure.
+//    Allocate and return a pointer to at least sz contiguous bytes of
+//    memory. Returns nullptr if sz == 0 or on failure.
 //
 //    The caller should initialize the returned memory before using it.
 //    The handout allocator sets returned memory to 0xCC (this corresponds
-//    to the x86 `int3` instruction and may help you debug).
+//    to the x86 int3 instruction and may help you debug).
 //
-//    If `sz` is a multiple of `PAGESIZE`, the returned pointer is guaranteed
+//    If sz is a multiple of PAGESIZE, the returned pointer is guaranteed
 //    to be page-aligned.
 //
 //    The handout code does not free memory and allocates memory in units
 //    of pages.
 void* kalloc(size_t sz) {
+
+    // Old kalloc() stuff
+    {
+    /*
     if (sz == 0 || sz > PAGESIZE) {
         return nullptr;
     }
-    static double te = 0;
 
     auto irqs = page_lock.lock();
     void* ptr = nullptr;
 
+    // skip over reserved and kernel memory
     auto range = physical_ranges.find(next_free_pa);
     while (range != physical_ranges.end()) {
         if (range->type() == mem_available) {
@@ -96,17 +123,99 @@ void* kalloc(size_t sz) {
         }
     }
 
+
     page_lock.unlock(irqs);
 
     if (ptr) {
         // tell sanitizers the allocated page is accessible
         asan_mark_memory(ka2pa(ptr), PAGESIZE, false);
-        // initialize to `int3`
+        // initialize to int3
         memset(ptr, 0xCC, PAGESIZE);
     }
-    else {
-        log_printf("[kalloc] FAILED to find available page.\n");
+    return ptr;
+    */ 
+    // IN PROGRESS CODE (BUDDY ALLOCATOR)
     }
+
+    if (sz == 0 || sz > (1 << MAX_ORDER)) {
+        log_printf("You dumb shit you asked for invalid memory\n");
+        return nullptr;
+    }
+
+    auto irqs = page_lock.lock();
+    void* ptr = nullptr;
+    uint64_t ord = order(sz);
+    log_printf("[kalloc] NEW request of size 0x%x, ord %lu\n", sz, ord);
+    ord = ord >= MIN_ORDER ? ord : MIN_ORDER; // min alloc is 1 page
+    log_printf("[kalloc] Actual ord given: %lu\n", ord);
+    bapg* blk = nullptr; // New block pointer
+
+    // If there are no free blocks of the desired order, then make one via looped block chopping.
+    if (!free_blocks[ord - MIN_ORDER].front()) { 
+        log_printf("[kalloc] FAILED to find avialable free block of order %lu, proceeding to create one\n", ord);
+        uint64_t nb_ord = ord; // next best order
+        
+        while (nb_ord < MAX_ORDER) { // finding next available higher order block
+            if (free_blocks[++nb_ord - MIN_ORDER].front()) {
+                bapg* new_blk = free_blocks[nb_ord - MIN_ORDER].pop_front();
+                log_printf("[kalloc] FOUND NBB of order %lu, sz 0x%x, addr 0x%x\n", 
+                    nb_ord, 1 << nb_ord, (new_blk-pgmap)*PAGESIZE);
+
+                long counter = 1;
+                while (nb_ord > ord) { // Iteratively break down block until order matches desired
+                    bapg* split_page = new_blk + (1 << (nb_ord - MIN_ORDER - 1));
+                    log_printf("[kalloc] SPLITTING at pa 0x%x (raw index %lu)\n", 
+                        (split_page-pgmap)*PAGESIZE, (split_page-pgmap));
+
+                    for (bapg* it = split_page; 
+                        it - split_page < (1 << (nb_ord - MIN_ORDER - 1)); 
+                        it++) { // Update the order for each page in the new child block 
+
+                        it->ord = nb_ord - 1;
+                    }
+
+                    // Push back the right child into the list of the given order.
+                    // The left child continues to get split until it is of the right order.
+                    free_blocks[nb_ord - MIN_ORDER - 1].push_back(split_page);
+                    assert(!free_blocks[nb_ord - MIN_ORDER - 1].empty());
+                    --nb_ord;
+                } // At this point, new_blk points to a block of the order we desire
+                log_printf("[kalloc] FINISHED splitting, new block of order %lu at addr 0x%x\n",
+                    nb_ord, (new_blk-pgmap)*PAGESIZE);
+                blk = new_blk;
+                break;
+            } 
+        }
+    } else { // Huzzah! There is already a block of the desired order free
+        log_printf("[kalloc] SUCCESS! Found new block of order %lu\n", ord);
+        blk = free_blocks[ord - MIN_ORDER].pop_front();
+    }
+
+    if (!blk) {
+        log_printf("[kalloc] Error: insufficient memory blocks available for requested size %lu\n", sz);
+        page_lock.unlock(irqs);
+        return nullptr;
+    }
+
+    // Compute the physical address of the block. Note that the index of pgmap is also the index
+    // of the page in physical memory! So the index is just the offset in physical memory
+    // in units of PAGESIZE's.
+    uint64_t addr = (blk - pgmap)*PAGESIZE;
+    ptr = pa2kptr<void*>(addr);
+    blk->returned = true;
+
+    for (bapg* it = blk; it - blk < (1 << (ord - MIN_ORDER)); it++) {
+        it->free = false;
+        it->ord = ord;
+    }
+
+    page_lock.unlock(irqs);
+
+    // Tell sanitizers the allocated page is accessible
+    asan_mark_memory(ka2pa(ptr), 1 << ord, false);
+    // Initialize to int3
+    memset(ptr, 0xCC, 1 << ord);
+    
     return ptr;
 }
 
--- k-memrange.hh
+++ k-memrange.hh
@@ -6,11 +6,11 @@ template <unsigned maxsize> class memrangeset;
 // struct encoding metadata for each page under the buddy allocator system.
 struct bapg {
     bool free = false; // whether the bapg is free or not
-    bool available = false; // if mem_available is set
     bool returned = false; // if the address of this page has been returned in kalloc()
     uint64_t ord; // order of the block
     uint64_t r_ord; // order of the root block of the current block
     uintptr_t r_addr; // address of the root block of the current block
+    list_links pglink;
 };
 
 // `memrangeset` stores type information for a range of memory addresses.
